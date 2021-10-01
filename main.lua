494fd8556295b8388fce293d68c0bc8556f0ac1d
28h Studio <leandro.libanio@28hstudio.com>
2021-10-01 13:36:15 -0300

    Project setup

---
+++ main.lua
@@ -0,0 +1,22 @@
+--[[
+    Asteroids game clone.
+    LibanioL 2021
+]]
+
+
+-- Game entry point
+function love.load()
+
+end
+
+
+-- Game loop
+function love.update(dt)
+
+end
+
+
+-- Game rendering
+function love.draw()
+
+end
